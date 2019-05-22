9225de3b3e38d31b777bc1f3f18d76eaa5f8dbfa
Nils46 <46447410+Nils46@users.noreply.github.com>
2019-05-22 12:28:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,14 +2,11 @@ from helpers import *
 
 # CONTENT BASED FILTERING MSE
 
-#test/training
 df = merge_reviews()
 training, test = split_data(df, d = 0.75)
 
-# make the utility matrix with the amount of stars
 utility = pivot_ratings(training)
 
-# create the dataframe with the business id's and categories
 df_1 = merge_businesses()
 categories_dataframe = extract_genres(df_1)
 
@@ -18,11 +15,9 @@ extracted = extract_genres(merge)
 pivot = pivot_genres(extracted)
 sim_categories = create_similarity_matrix_categories(pivot)
 
-# predict the ratings
 predicted_ratings = predict_ratings(sim_categories, utility, test)
 predicted_ratings.dropna()
 
-# calculate the mse for content based filtering
 mse_content = mse(predicted_ratings)
 print(mse_content)
 
@@ -30,11 +25,11 @@ print(mse_content)
 # ITEM BASED FILTERING MSE
 sim_ratings = create_similarity_matrix_cosine(utility)
 
-# predict ratings and calculate mse
 predicted_ratings_items = predict_ratings(sim_ratings, utility, test)
 mse_item_based = mse(predicted_ratings_items)
 
 print(mse_item_based)
+
 
 # DATA DENSITY
 
